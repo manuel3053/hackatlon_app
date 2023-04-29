ab7be95ec1c98d9f1b7087cd9c613210b4321f16
AndreSbro03 <andresbro03@gmail.com>
2023-04-29 15:18:39 +0200

    Ciao

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:hackatlon_app/Functions/triviaLogic.dart';
@@ -20,72 +22,100 @@ class _HomeState extends State<Home> {
   int level = 0;
   List<Quiz> game = generateTrivia(4);
 
-  List<Widget> gridItems = [
-    CircularPercentIndicator(
-      radius: 60,
-      lineWidth: 15,
-      percent: 60 / 100,
-      progressColor: Colors.red,
-      animation: true,
-      animationDuration: 1500,
-      center: Text('00%'),
-    ),
-    CircularPercentIndicator(
-      radius: 60,
-      lineWidth: 15,
-      percent: 60 / 100,
-      progressColor: Colors.red,
-      animation: true,
-      animationDuration: 1500,
-      center: Text('00% '),
-    ),
-    CircularPercentIndicator(
-      radius: 60,
-      lineWidth: 15,
-      percent: 60 / 100,
-      progressColor: Colors.red,
-      animation: true,
-      animationDuration: 1500,
-      center: Text('00%'),
-    ),
-    Padding(
-      padding: EdgeInsets.all(20),
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(40))),
-        child: Text('Wiki'),
-      ),
-    ),
-    Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: CustomPaint(
-        painter: Streak(),
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(''),
-              Text('Streak'),
-              Text('n'),
-            ],
-          ),
-        ),
-      ),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 150),
-      child: GridView.count(
-          padding: EdgeInsets.all(8),
-          crossAxisCount: 3,
-          crossAxisSpacing: 1.0,
-          mainAxisSpacing: 50.0,
-          children: gridItems),
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            CircularPercentIndicator(
+              radius: 60,
+              lineWidth: 15,
+              percent: 60 / 100,
+              progressColor: Colors.red,
+              animation: true,
+              animationDuration: 1500,
+              center: Text('00%'),
+            ),
+            CircularPercentIndicator(
+              radius: 60,
+              lineWidth: 15,
+              percent: 60 / 100,
+              progressColor: Colors.red,
+              animation: true,
+              animationDuration: 1500,
+              center: Text('00% '),
+            ),
+            CircularPercentIndicator(
+              radius: 60,
+              lineWidth: 15,
+              percent: 60 / 100,
+              progressColor: Colors.red,
+              animation: true,
+              animationDuration: 1500,
+              center: Text('00%'),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Text(
+                  'Wiki',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: CustomPaint(
+                    painter: Streak(),
+                    child: Container(
+                        height: 90,
+                        width: 70,
+                        child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(Icons.star),
+                                Text('120'),
+                              ],
+                            ))))),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Text(
+                  'Trivia',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
